5e40cc90a24308b36a73b635cee3e2d26a85fb34
Nikita Ipatov <Nikitafilemine1@gmail.com>
2023-09-21 23:01:58 +0300

    feat: Добавлены модели пользователей и фильмов

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Поле email обязательное'],
+    unique: [true, 'Пользователь с таким email уже существует'],
+    validate: {
+      validator: (email) => validator.isEmail(email),
+      message: 'Введите корректный email',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Поле password обязательное'],
+  },
+  name: {
+    type: String,
+    required: [true, 'Поле name обязательное'],
+    minlength: [2, 'Минимальная длина name - 2'],
+    maxlength: [30, 'Максимальная длина name - 30'],
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
