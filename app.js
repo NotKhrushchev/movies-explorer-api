ceb22a683602f0bcfea612610ba70c4d980b6c0d
Nikita Ipatov <Nikitafilemine1@gmail.com>
2023-09-21 22:55:10 +0300

    feat: Офоремлен корневой файл, добавлены файлы конфигурации

---
+++ app.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const { PORT = 3000 } = process.env;
+const { DATABASE_URL = 'mongodb://127.0.0.1:27017/movie-explorer-api' } = process.env;
+
+/** Точка входа */
+const app = express();
+
+/** Доступ обращения к ресурсам другого домена */
+app.use(cors());
+
+/** Дополнительная защита промежуточной обработки */
+app.use(helmet());
+
+/** Парсер данных в json */
+app.use(express.json());
+
+/** Подключение БД */
+mongoose.connect(DATABASE_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  autoIndex: true,
+});
+
+/** Установка порта */
+app.listen(PORT);
